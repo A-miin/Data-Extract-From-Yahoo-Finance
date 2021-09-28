f7c3c41775a10c1b50d357403eed777dd52e173a
A-miin <zulaykaisaeva@gmail.com>
2021-09-28 15:28:23 +0600

    Add Company model, serializer, view, url files

--- yahoo_scraping/models.py
+++ yahoo_scraping/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Company(models.Model):
+    date = models.DateField(_('Date'))
+    open = models.FloatField(_('Open'))
+    high = models.FloatField(_('High'))
+    low = models.FloatField(_('Low'))
+    close = models.FloatField(_('Close*'))
+    adj_close = models.FloatField(_('Adj Close**'))
+    volume = models.PositiveIntegerField(_('Volume'))
+
+    def __str__(self):
+        return f'{self.date.year}/{self.date.month}/{self.date.day}'
+
+    class Meta:
+        verbose_name = _('Company')
+        verbose_name_plural = _('Companies')
